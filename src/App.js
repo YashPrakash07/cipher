4c7af90e51a623510f3bd88f6c4df41501850a11
Yashprakash07 <Yashprakash07@gmail.com>
2023-04-22 11:08:12 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import Signup from "./pages/signup/Signup";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { CookiesProvider } from "react-cookie";
+
+import RequireUser from "./utils/RequireUser";
+import OnlyIfNotLogin from "./utils/OnlyIfNotLogin";
+import Followers from "./pages/followerPage/Followers";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <CookiesProvider>
+          <Routes>
+            <Route element={<RequireUser />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/userfollowers" element={<Followers />} />
+            </Route>
+            <Route element={<OnlyIfNotLogin />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Route>
+          </Routes>
+        </CookiesProvider>
+      </Provider>
     </div>
   );
 }
